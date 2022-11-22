dd2965cdd9eed9c8a960fb999c8c4f5863438a67
Vladislav Gogov <gogov.vladd@gmail.com>
2022-11-22 14:30:21 +0300

    Update main.cpp

--- tests/first/main.cpp
+++ tests/first/main.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
-int main(int arg, char *argv[]) 
-{
-    testing::InitGoogleTest(&arg, argv);
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
