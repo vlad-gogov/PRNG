33b4f883f231b26f6354060f429153bc19cb0f59
Vladislav Gogov <gogov.vladd@gmail.com>
2025-03-30 20:38:14 +0300

    MTAVX512 (#9)

--- generators/include/generators/mersenne_twister_simd.hpp
+++ generators/include/generators/mersenne_twister_simd.hpp
@@ -47,3 +47,45 @@ class MersenneTwister32AVX2 : public Generator<uint32_t> {
 };
 
 using MT32AVX2 = MersenneTwister32AVX2;
+
+class MersenneTwister32AVX512 : public Generator<uint32_t> {
+  private:
+    static constexpr size_t W = 32UL;
+    static constexpr size_t N = 624UL;
+    static constexpr size_t M = 397UL;
+    static constexpr size_t R = 31UL;
+    static constexpr size_t A = 0x9908b0dfUL;
+    static constexpr size_t U = 11UL;
+    static constexpr size_t D = 0xffffffffUL;
+    static constexpr size_t S = 7UL;
+    static constexpr size_t B = 0x9d2c5680UL;
+    static constexpr size_t T = 15UL;
+    static constexpr size_t C = 0xefc60000UL;
+    static constexpr size_t L = 18UL;
+    static constexpr size_t F = 1812433253UL;
+    static constexpr uint32_t default_seed = 5489u;
+    static constexpr uint32_t UPPER_MASK = (~uint32_t()) << R;
+    static constexpr uint32_t LOWER_MASK = ~UPPER_MASK;
+
+    alignas(64) std::array<uint32_t, N> mt;
+    size_t index_state;
+
+    void twist();
+
+    // Векторизованное преобразование tempering
+    __m512i tempering_simd(__m512i value) const;
+
+    // Скалярное преобразование tempering
+    uint32_t tempering_scalar(uint32_t value) const;
+
+  public:
+    MersenneTwister32AVX512(const uint32_t &seed = default_seed);
+
+    virtual uint32_t operator()() override;
+    virtual uint32_t min() const override;
+    virtual uint32_t max() const override;
+
+    void generate_bulk(uint32_t *output, size_t len);
+};
+
+using MT32AVX512 = MersenneTwister32AVX512;
--- generators/src/mersenne_twister_simd.cpp
+++ generators/src/mersenne_twister_simd.cpp
@@ -124,3 +124,125 @@ uint32_t MersenneTwister32AVX2::min() const {
 uint32_t MersenneTwister32AVX2::max() const {
     return std::numeric_limits<uint32_t>::max();
 }
+
+// TODO: Move in utils
+void print_m512(const __m512i &vec) {
+    uint32_t values[16];
+    _mm512_store_si512((__m512i *)values, vec);
+
+    for (int i = 0; i < 16; i++) {
+        std::cout << values[i] << " " << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+MersenneTwister32AVX512::MersenneTwister32AVX512(const uint32_t &seed) {
+    mt[0] = seed;
+    for (size_t i = 1; i < N; i++) {
+        mt[i] = (F * (mt[i - 1] ^ (mt[i - 1] >> (W - 2))) + i);
+    }
+    index_state = N;
+}
+
+__m512i MersenneTwister32AVX512::tempering_simd(__m512i y) const {
+    y = _mm512_xor_si512(y, _mm512_and_si512(_mm512_srli_epi32(y, U), _mm512_set1_epi32(D)));
+    y = _mm512_xor_si512(y, _mm512_and_si512(_mm512_slli_epi32(y, S), _mm512_set1_epi32(B)));
+    y = _mm512_xor_si512(y, _mm512_and_si512(_mm512_slli_epi32(y, T), _mm512_set1_epi32(C)));
+    y = _mm512_xor_si512(y, _mm512_srli_epi32(y, L));
+    return y;
+}
+
+uint32_t MersenneTwister32AVX512::tempering_scalar(uint32_t y) const {
+    y ^= (y >> U) & D;
+    y ^= (y << S) & B;
+    y ^= (y << T) & C;
+    y ^= (y >> L);
+    return y;
+}
+
+void MersenneTwister32AVX512::twist() {
+    const __m512i upper_mask = _mm512_set1_epi32(UPPER_MASK);
+    const __m512i lower_mask = _mm512_set1_epi32(LOWER_MASK);
+    const __m512i one = _mm512_set1_epi32(1);
+
+    size_t i = 0;
+    size_t j = M;
+    for (; i + 16 <= N && j + 16 <= N; i += 16, j += 16) {
+        __m512i mt_i = _mm512_load_si512((__m512i *)&mt[i]);
+        __m512i mt_i1 = _mm512_loadu_si512((__m512i *)&mt[i + 1]);
+        __m512i x = _mm512_or_si512(_mm512_and_si512(mt_i, upper_mask), _mm512_and_si512(mt_i1, lower_mask));
+
+        __m512i xA = _mm512_srli_epi32(x, 1);
+
+        __mmask16 mask = _mm512_test_epi32_mask(x, one);
+        xA = _mm512_mask_xor_epi32(xA, mask, xA, _mm512_set1_epi32(A));
+
+        __m512i mt_im = _mm512_loadu_si512((__m512i *)&mt[j]);
+        __m512i res = _mm512_xor_si512(mt_im, xA);
+
+        _mm512_storeu_si512((__m512i *)&mt[i], res);
+    }
+    for (; i + 16 <= N && j < N; j++, i++) {
+        size_t x = (mt[i] & UPPER_MASK) | (mt[(i + 1) % N] & LOWER_MASK);
+        mt[i] = mt[j] ^ (x >> 1) ^ ((x & 1) ? A : 0);
+    }
+    j = 0;
+    for (; i + 16 <= N && j + 16 <= N; i += 16, j += 16) {
+        __m512i mt_i = _mm512_loadu_si512((__m512i *)&mt[i]);
+        __m512i mt_i1 = _mm512_loadu_si512((__m512i *)&mt[i + 1]);
+        __m512i x = _mm512_or_si512(_mm512_and_si512(mt_i, upper_mask), _mm512_and_si512(mt_i1, lower_mask));
+
+        __m512i xA = _mm512_srli_epi32(x, 1);
+
+        __mmask16 mask = _mm512_test_epi32_mask(x, one);
+        xA = _mm512_mask_xor_epi32(xA, mask, xA, _mm512_set1_epi32(A));
+
+        __m512i mt_im = _mm512_loadu_si512((__m512i *)&mt[j]);
+        __m512i res = _mm512_xor_si512(mt_im, xA);
+
+        _mm512_storeu_si512((__m512i *)&mt[i], res);
+    }
+
+    for (; i < N; i++) {
+        size_t x = (mt[i] & UPPER_MASK) | (mt[(i + 1) % N] & LOWER_MASK);
+        mt[i] = mt[(i + M) % N] ^ (x >> 1) ^ ((x & 1) ? A : 0);
+    }
+    index_state = 0;
+}
+
+uint32_t MersenneTwister32AVX512::operator()() {
+    if (index_state >= N) {
+        twist();
+    }
+    return tempering_scalar(mt[index_state++]);
+}
+
+void MersenneTwister32AVX512::generate_bulk(uint32_t *output, size_t len) {
+    size_t j = 0;
+    size_t i = 0;
+    for (i = 0, j = 0; i + 16 <= len; i += 16, j += 16) {
+        if (index_state >= N) {
+            twist();
+            j = 0;
+        }
+        __m512i data = _mm512_load_si512((__m512i *)&mt[j]);
+        _mm512_storeu_si512((__m512i *)&output[i], tempering_simd(data));
+        index_state += 16;
+    }
+    for (; i < len; i++, j++) {
+        if (index_state >= N) {
+            twist();
+            j = 0;
+        }
+        output[i] = tempering_scalar(mt[j]);
+        index_state += 1;
+    }
+}
+
+uint32_t MersenneTwister32AVX512::min() const {
+    return 0u;
+}
+
+uint32_t MersenneTwister32AVX512::max() const {
+    return std::numeric_limits<uint32_t>::max();
+}
