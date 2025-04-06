bbe3f9d7e85da3db1f4cc1eb32e937dbac4a5ff7
vlad-gogov <gogov.vladd@gmail.com>
2025-04-06 12:09:23 +0300

    fix code style

--- tests/MT/mt_tests.cpp
+++ tests/MT/mt_tests.cpp
@@ -2,6 +2,7 @@
 
 #include <generators/base_error.hpp>
 #include <generators/mersenne_twister.hpp>
+#include <generators/mersenne_twister_sbox.hpp>
 #include <generators/mersenne_twister_simd.hpp>
 #include <metrics/nist_tests.hpp>
 
@@ -1120,3 +1121,216 @@ TEST(MT32AVX512, can_generate_correct_seq) {
     }
 }
 #endif
+
+TEST(MTSBOX, frequency_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::frequency_test(bytes);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_frequency_test(bytes));
+}
+
+TEST(MTSBOX, frequency_block_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::frequency_block_test(bytes, 128);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_frequency_block_test(bytes, 128));
+}
+
+TEST(MTSBOX, runs_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::runs_test(bytes);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_runs_test(bytes));
+}
+
+TEST(MTSBOX, longest_run_of_ones_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::longest_run_of_ones(bytes);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_longest_run_of_ones(bytes));
+}
+
+TEST(MTSBOX, binary_matrix_rank_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    size_t M = 32;
+    size_t Q = 32;
+    double p = nist::binary_matrix_rank(bytes, M, Q);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_binary_matrix_rank(bytes, M, Q));
+}
+
+TEST(MTSBOX, discrete_fourier_transform_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::discrete_fourier_transform(bytes);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_discrete_fourier_transform(bytes));
+}
+
+TEST(MTSBOX, non_overlapping_template_matching_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    utils::seq_bytes template_ = {0, 0, 0, 0, 0, 0, 0, 0, 1};
+    double p = nist::non_overlapping_template_matching(bytes, template_);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_non_overlapping_template_matching(bytes, template_));
+}
+
+TEST(MTSBOX, overlapping_template_matching_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    utils::seq_bytes template_ = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    double p = nist::overlapping_template_matching(bytes, template_);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_FALSE(nist::check_overlapping_template_matching(bytes, template_));
+}
+
+TEST(MTSBOX, universal_digit_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::universal(bytes);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_universal(bytes));
+}
+
+TEST(MTSBOX, serial_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    auto [p1, p2] = nist::serial_complexity(bytes, 16);
+    std::cout << "P-value: " << p1 << " " << p2 << std::endl;
+    ASSERT_TRUE(nist::check_serial_complexity(bytes, 16));
+}
+
+TEST(MTSBOX, approximate_entropy_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::approximate_entropy(bytes, 10);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_approximate_entropy(bytes, 10));
+}
+
+TEST(MTSBOX, cumulative_sums_mt_forward) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::cumulative_sums(bytes, nist::CumulativeSumsMode::Forward);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_cumulative_sums(bytes, nist::CumulativeSumsMode::Forward));
+}
+
+TEST(MTSBOX, cumulative_sums_mt_reverse) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    double p = nist::cumulative_sums(bytes, nist::CumulativeSumsMode::Reverse);
+    std::cout << "P-value: " << p << std::endl;
+    ASSERT_TRUE(nist::check_cumulative_sums(bytes, nist::CumulativeSumsMode::Reverse));
+}
+
+TEST(MTSBOX, random_excursions_mt) {
+    MT19937SBOX generator(12345);
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    std::vector<double> p_values = nist::random_excursions(bytes);
+    std::cout << "P-values: ";
+    for (size_t i = 0; i < p_values.size(); i++) {
+        std::cout << p_values[i] << " ";
+    }
+    std::cout << std::endl;
+    std::vector<bool> answers = nist::check_random_excursions(bytes);
+    for (const auto &answer : answers) {
+        ASSERT_TRUE(answer);
+    }
+}
+
+TEST(MTSBOX, random_excursions_variant_mt) {
+    MT19937SBOX generator;
+    std::uint32_t count_number = 16384u;
+    std::vector<std::uint32_t> numbers(count_number);
+    for (std::uint32_t i = 0; i < count_number; ++i) {
+        numbers[i] = generator();
+    }
+    utils::seq_bytes bytes = utils::convert_numbers_to_seq_bytes(numbers);
+    std::vector<double> p_values = nist::random_excursions_variant(bytes);
+    std::cout << "P-values: ";
+    for (size_t i = 0; i < p_values.size(); i++) {
+        std::cout << p_values[i] << " ";
+    }
+    std::cout << std::endl;
+    std::vector<bool> answers = nist::check_random_excursions_variant(bytes);
+    for (const auto &answer : answers) {
+        ASSERT_TRUE(answer);
+    }
+}
